46d26214bc236078dcbee5aaa722de4f65d18edf
Victor Fregni <devfgog@gmail.com>
2024-10-17 21:03:09 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Contador from './components/Contador'
+import Relogio from './components/Relogio'
+
+function App() {
+ 
+
+  return (
+    <>
+      <Relogio/>
+      <Contador/>
+    </>
+  )
+}
+
+export default App

9746b417bdc2c93ab52fd023e0d9f7af5a166a2a
Victor Fregni <devfgog@gmail.com>
2024-10-17 21:21:29 -0300

    Add files via upload

--- src/components/Relogio.jsx
+++ src/components/Relogio.jsx
@@ -1,17 +1,20 @@
 import { useEffect, useState } from "react"
 
 function Relogio(){
+    const dia = useState(new Date().toLocaleDateString());
     const [hora, setHora] = useState(new Date().toLocaleTimeString());
     useEffect(()=>{
         const interval = setInterval(() => {
         setHora(new Date().toLocaleTimeString());
-    }, 1000)
-    return() => {clearInterval(interval);};
+    }, 1000);
+    return() => clearInterval(interval);
 },[]);
     return(
         <>
         <h1>Hora Agora</h1>
         <h2>{hora}</h2>
+        <h1>Dia Hoje</h1>
+        <h2>{dia}</h2>
         </>
     )
 }
